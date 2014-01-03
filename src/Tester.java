8a7c36958742d6ccbdd7bf10e1a682bbb302d069
Mustafa Akin <mustafa91@gmail.com>
2014-01-03 17:33:59 +0200

    Eucledian and Manhattan distance implemented for comparison

--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,42 @@
 public class Tester {
+	public static Point eucledian(Point[] points, Point reference){
+		int N = points.length;
+		
+		double min = Double.MAX_VALUE;
+		Point result = null;
+		for(int i = 0; i < N; i++){
+			double value = 0;
+			Point p = points[i];
+			for(int j = 0; j < reference.values.length; j++){
+				value += (p.values[j] - reference.values[j]) * (p.values[j] - reference.values[j]) ;				
+			}
+			if ( value < min){
+				min = value;
+				result = p;						
+			}
+		}	
+		return result;
+	}
 
+	public static Point manhattan(Point[] points, Point reference){
+		int N = points.length;
+		
+		double min = Double.MAX_VALUE;
+		Point result = null;
+		for(int i = 0; i < N; i++){
+			double value = 0;
+			Point p = points[i];
+			for(int j = 0; j < reference.values.length; j++){
+				value += Math.abs((p.values[j] - reference.values[j])) ;				
+			}
+			if ( value < min){
+				min = value;
+				result = p;					
+			}
+		}	
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		Point p1 = new Point(0.5, 0.9, 0.6, 0.7);
 		Point p2 = new Point(0.2, 0.6, 0.4, 0.6);
@@ -14,11 +51,15 @@ public class Tester {
 		IGrid iGrid = new IGrid(ps);
 		iGrid.printDatabase();
 
-		Point pTest = new Point(0.45, 0.4, 0.3, 0.6);
+		Point pTest = new Point(0.6, 0.1, 0.4, 0.6);
 		Range[] r = iGrid.searchDimensions(pTest);
 		for (int i = 0; i < r.length; i++) {
 			System.out.println("Dimension " + i + " " + r[i]);
 		}
+		
+		System.out.println("Eucledian : " + eucledian(ps, pTest));
+		System.out.println("Manhattan : " + manhattan(ps, pTest));
+
 	}
 
 }
